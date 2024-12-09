3cf788986bda8821f4903dbaa1742100a9256032
fatimaezzahraelembarek <fatimaezzahraelembarek21@gmail.com>
2024-12-09 10:16:12 +0100

    Initial Commit

---
+++ drinkselector2/drinkselector2App.swift
@@ -0,0 +1,17 @@
+//
+//  drinkselector2App.swift
+//  drinkselector2
+//
+//  Created by Fatima Ezzahrae LEMBAREK on 09/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct drinkselector2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
